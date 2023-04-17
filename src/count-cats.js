984a590a2be5fa8a3cb16e6d19faa2690c0c3635
magichano <antonartamonov1@gmail.com>
2023-04-17 23:14:24 +0300

    feat: add solutions 1 and 3 tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,22 +1,19 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Given matrix where you have to find cats by ears "^^"
- *
- * @param {Array<Array>} matrix 
- * @return {Number} count of cats found
- *
- * @example
- * countCats([
- *  [0, 1, '^^'],
- *  [0, '^^', 2],
- *  ['^^', 1, 2]
- * ]) => 3`
- *
- */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let count = 0;
+
+
+
+  for (let i of matrix) {
+    for (let j of i) {
+      if (j == '^^') {
+        count++
+      }
+    }
+  }
+
+  return (count)
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,21 +1,32 @@
-const { NotImplementedError } = require('../extensions/index.js');
+function createDreamTeam(members) {
+  let stringArray=[]
+  let numberArray=[]
 
-/**
- * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
- * @return {String | Boolean} name of the team or false
- * in case of incorrect members
- *
- * @example
- * 
- * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
- * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
- *
- */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  
+  for (i of members){
+
+    if ((typeof(i)=='string')) {
+      i=i.replace(/\s/g, '')
+      numberArray.push(i[0].toUpperCase().charCodeAt(0))
+    }
+  }
+
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+  
+  numberArray.sort(compareNumeric);
+
+  for (i of numberArray){
+      stringArray.push(String.fromCharCode(i));
+  }
+
+  return (stringArray.join(''))
 }
 
 module.exports = {
